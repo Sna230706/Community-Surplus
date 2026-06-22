5bb5b4eb2ba5925c270cf23d8838d238a9fc63ae
Anagha Anilkumar <mdl24cs032@mec.ac.in>
2026-06-22 16:09:07 +0530

    Add basic products API routes using Express

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,41 @@
 const express=require("express");
 const app=express();
-app.get("/",(req,res) => {
-    res.send("Backend is working!");
+
+app.use(express.json());
+
+let products=[
+    {
+        id:1,
+        name:"Laptop",
+        price:15000,
+        seller:"John"
+    }
+];
+
+app.get("/api/products", (req,res) => {
+    res.json(products);
 });
+
+app.post("/api/products", (req,res) => {
+    const product=req.body;
+    products.push(product);
+    res.json({
+        message:"Product added successfully",
+        product
+    });
+
+});
+
+
+app.get("/api/products/:id", (req,res) => {
+   
+    const id=parseInt(req.params.id);
+    const product=products.find( p => p.id === id);
+    
+    res.json(product);
+});
+
 app.listen(5000,() => {
- console.log("Server running on port 5000");
+    console.log("Server is running on port 5000");
 });
+
